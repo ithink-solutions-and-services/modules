dc773a48c95941149d7ba555b8c617f680d41bb1
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-10 23:49:03 +0800

    Add files via upload

---
+++ scaffold/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2016 brain-tec AG (http://www.braintec-group.com)
+#    All Right Reserved
+#
+#    See LICENSE file for full licensing details.
+##############################################################################
+
+from . import controllers
+from . import models
