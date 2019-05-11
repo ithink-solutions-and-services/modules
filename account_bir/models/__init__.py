4dd225ee1e64f28c49e95974849f1f0de2f6dae9
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-11 16:42:59 +0800

    Update __init__.py

--- account_bir/models/__init__.py
+++ account_bir/models/__init__.py
@@ -1,10 +1,1 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (c) 2016 brain-tec AG (http://www.braintec-group.com)
-#    All Right Reserved
-#
-#    See LICENSE file for full licensing details.
-##############################################################################
-
-from . import models
+from . import account_bir
