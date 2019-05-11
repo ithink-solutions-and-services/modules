24ac9fced39ce5f00c81ffbe7fa38c3be868ac22
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-11 19:47:46 +0800

    Update account_bir.py

--- account_bir/models/account_bir.py
+++ account_bir/models/account_bir.py
@@ -13,6 +13,28 @@ class AccountBir(models.Model):
     code = fields.Char("Code")
     active = fields.Boolean("Active", default=True)
     wizard_id = fields.Char("Wizard ID")
+
+    
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    barangay = fields.Char("Barangay")
+    district = fields.Char("District/Municipality")
+    rdo = fields.Char("RDO Code")
+    branch_code = fields.Char(string="Branch Code", default="0000")
+    line_of_business = fields.Char("Line of Business")
+    taxpayer_classification = fields.Selection([('ind','Individual'),('corp','Non-Individual')],string="Taxpayer Classification")
+    category_of_withholding = fields.Selection([('private','Private'),('government','Government')],string="Category of Withholding Agent")
+    foreign_address = fields.Char("Foreign Address")
+    foreign_zip = fields.Char("Foreign Zip Code")
+
+    def strip_space(self, string):
+        return " ".join(string.split() if string != False else '')
+    
+    firstname = fields.Char(string="Firstname")
+    middlename = fields.Char(string="Middlename")
+    lastname = fields.Char(string="Lastname")
+    tradename = fields.Char(string="Trade Name")
     
     
 class ResCompany(models.Model):
