255890c406aae1c9b19a54034f3160767c49e32f
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-11 17:04:29 +0800

    Create account_bir_forms_wizard.py

---
+++ account_bir/wizard/account_bir_forms_wizard.py
@@ -0,0 +1,12 @@
+from odoo import models, api
+
+
+class AccountBirForms(models.TransientModel):
+    _name = "account.bir.forms"
+    
+    account_bir_id = fields.Many2one('account.bir', string="BIR Form")
+    
+    @api.multi
+    def select_form(self):
+        self.ensure_one()
+        return self.env.ref('self.account_bir_id.wizard_id')
