a1ba1fa1c7f37a7a041be90f5561da94ae99fa1d
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-12 01:14:27 +0800

    Delete account_bir.py

--- account_bir_forms_slspi/models/account_bir.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class AccountBir(models.Model):
-    _name = 'account.bir'
-    
-    name = fields.Char("BIR Form")
-    code = fields.Char("Code")
-    active = fields.Boolean("Active", default=True)
-    wizard_id = fields.Char("Wizard ID")
