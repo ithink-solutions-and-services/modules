8e9b8c772ad0d133162420b22904ba533e91aa2e
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-11 17:38:03 +0800

    Update __manifest__.py

--- account_bir_forms_slspi/__manifest__.py
+++ account_bir_forms_slspi/__manifest__.py
@@ -1,18 +1,20 @@
 {
-    'name': "BIR Forms for Accounting",
+    'name': "BIR Summary List of Sales, Purchases, and Importations",
     'author': "Nathaniel Lew Aquino",
     'license': 'OPL-1',
     'version': '1.0',
-    'summary': "BIR Philippines Forms Generation",
+    'summary': "Module to enable generation of Summary List of Sales, Purchases, and Importations",
     'description': """
-        Base module for BIR Forms Generation
+        Module to enable generation of Summary List of Sales, Purchases, and Importations
     """,
     'category': 'Accounting',
     'website': 'https://ithinksols.com',
-    'depends': ['base', 'account_invoicing'],
-    'data': ['security/ir.model.access.csv',
-             'views/account_bir_views.xml.xml',
-             'wizard/account_bir_forms_wizard_views.xml'
+    'depends': ['base', 'account_bir'],
+    'data': [
+            #'security/ir.model.access.csv',
+             'data/account_bir_forms_slspi_data.xml',
+             #'views/account_bir_views.xml.xml',
+            # 'wizard/account_bir_forms_wizard_views.xml'
              ],
     'installable': True,
     'application': False,
