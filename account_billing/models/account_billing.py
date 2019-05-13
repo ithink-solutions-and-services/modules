f933f519cc2e63bae3983231b4f7fa75b259f5eb
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-13 21:08:26 +0800

    Update account_billing.py

--- account_billing/models/account_billing.py
+++ account_billing/models/account_billing.py
@@ -16,6 +16,7 @@ class AccountBilling(models.Model):
     date_start = fields.Date(string='Date Started', default=fields.Date.today)
     date_closed = fields.Date(string='Date Closed', track_visibility='onchange')
     billing_line_ids = fields.One2many('account.billing.line', 'billing_id', string="Billing Lines")
+    billing_period_id = fields.Many2one('account.billing.period', string="Billing Period")
     
     @api.depends('billing_line_ids')
     def _total(self):
@@ -33,7 +34,30 @@ class AccountBilling(models.Model):
     suspend_reason = fields.Text("Reason for Suspension/Closure/Cancellation", track_visibility='onchange')
     user_id = fields.Many2one('res.users', string='Assigned User', track_visibility='onchange')
     invoice_ids = fields.One2many('account.invoice', 'billing_id', string="Invoices")
+    template_id = fields.Many2one('account.billing.template', string="Billing Template")
     
+    @api.onchange('template_id')
+    def _onchange_template_id(self):
+        for rec in self:
+            if rec.template_id and rec.state == 'draft':
+                rec.recurring_type = rec.template_id.recurring_type
+                rec.recurring_type_interval = rec.template_id.recurring_type_interval
+                rec.billing_line_ids.sudo().unlink()
+                for line_id in rec.template_id.template_line_ids:
+                    vals = {
+                        'billing_id': rec.id,
+                        'product_id': line_id.product_id.id,
+                        'quantity': line_id.quantity
+                    }
+                    self.env['account.billing.line'].sudo().create(vals)
+    
+    
+    @api.depends('invoice_ids')
+    def _compute_invoice_count(self):
+        for rec in self:
+            rec.invoice_count = len(rec.invoice_ids)
+            
+    invoice_count = fields.Integer(compute='_compute_invoice_count', store=True)
     
 class AccountBillingLine(models.Model):
     _name = 'account.billing.line'
@@ -61,3 +85,48 @@ class AccountBillingLine(models.Model):
     taxed_price = fields.Float('Taxed Price', compute="_product", store=True)
     subtotal = fields.Float('Subtotal', compute="_product", store=True)
     
+class AccountBillingPeriod(models.Model):
+    _name = 'account.billing.period'
+    
+    name = fields.Char("Billing Period")
+    date_start = fields.Date("Date Start")
+    date_end = fields.Date("Date End")
+    
+class AccountBillingTemplate(models.Model):
+    _name = 'account.billing.template
+    
+    name = fields.Char("Template Name")
+    template_line_ids = fields.One2many('account.billing.template.line', 'template_id', string="Template Lines")
+    recurring_type = fields.Selection([('daily', 'Day(s)'), ('weekly', 'Week(s)'),
+                                            ('monthly', 'Month(s)'), ('yearly', 'Year(s)'), ],
+                                           string='Recurrency',
+                                           help="Invoice automatically repeat at specified interval",
+                                           default='monthly', track_visibility='onchange')
+    recurring_type_interval = fields.Integer(string="Repeat Every", help="Repeat every (Days/Week/Month/Year)", default=1, track_visibility='onchange')
+    
+class AccountBillingTemplateLine(models.Model):
+    _name = 'account.billing.template.line'
+    
+    template_id = fields.Many2one('account.billing.template', string="Linked to Template")
+    product_id = fields.Many2one('product.product', string="Product")
+    quantity = fields.Float('Quantity', default=1)
+    
+    @api.depends('product_id', 'quantity')
+    def _product(self):
+        for rec in self:
+            unit_price = 0
+            subtotal = 0
+            if rec.product_id:
+                unit_price = rec.product_id.lst_price
+                tax_price = 0
+                for tax in rec.product_id.taxes_id:
+                    if tax.amount_type == 'percent':
+                        tax_price = tax_price + unit_price * (tax.amount/100)
+                rec.unit_price = unit_price
+                rec.taxed_price = unit_price + tax_price
+                rec.subtotal = rec.unit_price * rec.quantity
+    
+    unit_price = fields.Float('Unit Price(non-VAT)', compute="_product", store=True)
+    taxed_price = fields.Float('Taxed Price', compute="_product", store=True)
+    subtotal = fields.Float('Subtotal', compute="_product", store=True)
+                                        
