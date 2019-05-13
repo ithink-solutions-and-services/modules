a7a298b1dcfc39b16f134d7b38a381964246feba
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-13 21:20:26 +0800

    Update account_billing.py

--- account_billing/models/account_billing.py
+++ account_billing/models/account_billing.py
@@ -93,7 +93,99 @@ class AccountBilling(models.Model):
 
     @api.multi
     def set_close(self):
-        return self.write({'state': 'close', 'date': fields.Date.from_string(fields.Date.today())})
+        for rec in self:
+            rec.write({'state': 'close', 'date': fields.Date.from_string(fields.Date.today())})
+        return True
+    
+    @api.multi
+    def _prepare_invoice_data(self):
+        self.ensure_one()
+
+        if not self.partner_id:
+            raise UserError(_("You must first select a Customer for Billing %s!") % self.name)
+
+        company_id = self.partner_id.company_id
+
+        fpos_id = self.env['account.fiscal.position'].get_fiscal_position(self.partner_id.id)
+        journal = self.template_id.journal_id or self.env['account.journal'].search([('type', '=', 'sale'), ('company_id', '=', company.id)], limit=1)
+        if not journal:
+            raise UserError(_('Please define a sale journal for the company "%s".') % (company.name or '', ))
+
+        next_date = fields.Date.from_string(self.recurring_next_date)
+        periods = {'daily': 'days', 'weekly': 'weeks', 'monthly': 'months', 'yearly': 'years'}
+        end_date = next_date + relativedelta(**{periods[self.recurring_type]: self.recurring_interval})
+        end_date = end_date - relativedelta(days=1)     # remove 1 day as normal people thinks in term of inclusive ranges.
+        # DO NOT FORWARDPORT
+        format_date = self.env['ir.qweb.field.date'].value_to_html
+
+        return {
+            'account_id': self.partner_id.property_account_receivable_id.id,
+            'type': 'out_invoice',
+            'partner_id': self.partner_id.id,
+            'currency_id': company_id.currency_id.id,
+            'journal_id': journal.id,
+            'origin': self.name,
+            'fiscal_position_id': fpos_id,
+            'payment_term_id': self.partner_id.property_payment_term_id.id,
+            'company_id': company.id,
+            'comment': _("This invoice covers the following period: %s - %s") % (format_date(fields.Date.to_string(next_date), {}), format_date(fields.Date.to_string(end_date), {})),
+            'user_id': self.user_id.id,
+            'billing_id': self.id,
+            'billing_period': self.billing_period_id.id,
+            'billing_template_id': self.template_id.id
+        }
+
+    @api.multi
+    def _prepare_invoice_line(self, line, fiscal_position):
+        if 'force_company' in self.env.context:
+            company = self.env['res.company'].browse(self.env.context['force_company'])
+        else:
+            company = line.analytic_account_id.company_id
+            line = line.with_context(force_company=company.id, company_id=company.id)
+
+        account = line.product_id.property_account_income_id
+        if not account:
+            account = line.product_id.categ_id.property_account_income_categ_id
+        account_id = fiscal_position.map_account(account).id
+
+        tax = line.product_id.taxes_id.filtered(lambda r: r.company_id == company)
+        tax = fiscal_position.map_tax(tax)
+
+        return {
+            'name': line.name + " - " + line.months,
+            'account_id': account_id,
+            'account_analytic_id': line.analytic_account_id.analytic_account_id.id,
+            'price_unit': line.price_unit or 0.0,
+            'discount': line.discount,
+            'quantity': line.quantity,
+            'uom_id': line.uom_id.id,
+            'product_id': line.product_id.id,
+            'invoice_line_tax_ids': [(6, 0, tax.ids)],
+        }
+
+    @api.multi
+    def _prepare_invoice_lines(self, fiscal_position):
+        self.ensure_one()
+        fiscal_position = self.env['account.fiscal.position'].browse(fiscal_position)
+        line_ids = []
+        for line in self.recurring_invoice_line_ids:
+            if line.months_total == 0:
+#                line.name = line.name + " - " + line.months
+                line_ids.append((0, 0, self._prepare_invoice_line(line, fiscal_position)))
+            else:
+                if line.months_total > line.months_current:
+                    line.months_current += 1
+#                    line.name = line.name + " - " + line.months
+                    line_ids.append((0, 0, self._prepare_invoice_line(line, fiscal_position)))
+        return line_ids
+
+#        return [(0, 0, self._prepare_invoice_line(line, fiscal_position)) for line in self.recurring_invoice_line_ids]
+
+    @api.multi
+    def _prepare_invoice(self):
+        invoice = self._prepare_invoice_data()
+        invoice['invoice_line_ids'] = self._prepare_invoice_lines(invoice['fiscal_position_id'])
+        return invoice
     
 class AccountBillingLine(models.Model):
     _name = 'account.billing.line'
@@ -132,6 +224,7 @@ class AccountBillingTemplate(models.Model):
     _name = 'account.billing.template
     
     name = fields.Char("Template Name")
+    journal_id = fields.Many2one('account.jounal', string="Sales Journal")
     template_line_ids = fields.One2many('account.billing.template.line', 'template_id', string="Template Lines")
     recurring_type = fields.Selection([('daily', 'Day(s)'), ('weekly', 'Week(s)'),
                                             ('monthly', 'Month(s)'), ('yearly', 'Year(s)'), ],
