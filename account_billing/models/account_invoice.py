e4d555976097eab8bc3dbcf8a7058c08e2ea723b
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-13 21:21:34 +0800

    Update account_invoice.py

--- account_billing/models/account_invoice.py
+++ account_billing/models/account_invoice.py
@@ -8,3 +8,5 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
     
     billing_id = fields.Many2one('account.billing', string="Linked to Billing")
+    billing_period_id = fields.Many2one('account.billing.period', string="Billing Period")
+    billing_template_id = fields.Many2one('account.billing.template', string="Billing Template")
