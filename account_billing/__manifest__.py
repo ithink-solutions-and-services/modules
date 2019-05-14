de70a9b947d5aa4bba6f821d1ee3b422c3129c4d
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-14 16:45:29 +0800

    Update __manifest__.py

--- account_billing/__manifest__.py
+++ account_billing/__manifest__.py
@@ -7,10 +7,9 @@
     'category': 'Billing',
     'website': 'www.ithinksols.com',
     'depends': ['base', 'account'],
-    'data': [
+    'data': ['security/ir.model.access.csv',
              'data/ir_sequence_data.xml',
              'data/ir_actions_server_data.xml',
-
              'views/account_billing_views.xml',
              ],
     'installable': True,
