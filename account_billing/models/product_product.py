003c6a7610371aa404184cc0fead14281d96e729
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-14 14:32:01 +0800

    Update product_product.py

--- account_billing/models/product_product.py
+++ account_billing/models/product_product.py
@@ -5,7 +5,7 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
     
     water_product = fields.Boolean("Water Consumption?", default=False)
-    cu_m_fixed = fields.Integer("Fixed Rate cu. m")
+    cu_m_fixed = fields.Integer("Fixed cu. m")
     cu_m_fixed_price = fields.Float("Fixed Price")
     cu_m_exceed_price = fields.Float("Exceeding Price per cu. m")
     
