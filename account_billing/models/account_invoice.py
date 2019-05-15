85bf17c05d992b78c88fefefb19da5c6de557318
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-15 16:23:35 +0800

    Update account_invoice.py

--- account_billing/models/account_invoice.py
+++ account_billing/models/account_invoice.py
@@ -10,6 +10,16 @@ class AccountInvoice(models.Model):
     billing_id = fields.Many2one('account.billing', string="Linked to Billing")
     billing_period_id = fields.Many2one('account.billing.period', string="Billing Period")
     billing_template_id = fields.Many2one('account.billing.template', string="Billing Template")
+    
+    
+    @api.depends('invoice_line_ids', 'invoice_line_ids.cu_m', 'invoice_line_ids.prev_cu_m')
+    def _cu_m(self):
+        for rec in self:
+            rec.cu_m_total = sum(rec.invoice_line_ids.mapped("cu_m"))
+            rec.prev_cu_m_total = sum(rec.invoice_line_ids.mapped("prev_cu_m"))
+    
+    cu_m_total = fields.Float("Total Cu. M", compute="_cu_m", store=True)
+    prev_cu_m_total = fields.Float("Total Previous Cu. M", compute="_cu_m", store=True)
 
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
