98229e171c6867b5bb0edccf001b4bb0ab8dbf75
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-15 19:27:42 +0800

    Update account_invoice.py

--- account_billing/models/account_invoice.py
+++ account_billing/models/account_invoice.py
@@ -20,6 +20,15 @@ class AccountInvoice(models.Model):
     
     cu_m_total = fields.Float("Total Cu. M", compute="_cu_m", store=True)
     prev_cu_m_total = fields.Float("Total Previous Cu. M", compute="_cu_m", store=True)
+    
+    @api.depends('invoice_line_ids', 'invoice_line_ids.price_subtotal', 'invoice_ids.product_id.water_product', 'invoice_ids.product_id.monthly_due_product')
+    def _water_total(self):
+        for rec in self:
+            rec.water_total = sum(rec.invoice_line_ids.filtered(lambda r: r.water_product == True).mapped("price_subtotal"))
+            rec.monthly_due_total = sum(rec.invoice_line_ids.filtered(lambda r: r.monthly_due_product == True).mapped("price_subtotal"))
+    
+    water_total = fields.Float("Total Water Consumption", compute="_water_total", store=True)
+    monthly_due_total = fields.Float("Total Monthly Dues", compute="_water_total", store=True)
 
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
