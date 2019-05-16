633e2a27469bfc808ae7c0b5bdace1b021106b7f
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-16 09:56:21 +0800

    Update account_billing_generate.py

--- account_billing/wizard/account_billing_generate.py
+++ account_billing/wizard/account_billing_generate.py
@@ -10,4 +10,4 @@ class AccountBillingGenerate(models.TransientModel):
         ids = []
         for invoice in invoices:
             ids.append(invoice.id)
-        return self.env.ref('account.account_invoices').report_action(ids)
+        return self.env.ref('account_billing.report_account_invoice_bill_report').report_action(ids)
