079bc5a84cd07554719814e6af2436236eb5f624
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-17 10:07:13 +0800

    Update account_billing_dashboard.py

--- account_billing/models/account_billing_dashboard.py
+++ account_billing/models/account_billing_dashboard.py
@@ -41,16 +41,34 @@ class AccountBillingDashboard(models.Model):
                     if count > 0:
                         rec.sudo().unlink()
         else:
-            self.env['account.billing.dashboard'].sudo().create({'name': 'Dashboard 1', 'type': 'combined', 'content_type': 'col_day'})
+            self.env['account.billing.dashboard'].sudo().create({'name': 'Dashboard 1', 'type': 'text', 'content_type': 'col_day'})
+        if records:
+            if len(records) == 0:
+                self.env['account.billing.dashboard'].sudo().create({'name': 'Dashboard 2'})
+            elif len(records) > 1:
+                count = 0
+                for rec in records:
+                    if count > 0:
+                        rec.sudo().unlink()
+        else:
+            self.env['account.billing.dashboard'].sudo().create({'name': 'Dashboard 2', 'type': 'combined', 'content_type': 'col_mo'})
         self.env['account.billing.dashboard'].sudo().search([('content_type','=',False)]).unlink()
                     
                     
-
+    @api.one
     def _count(self):
-        for rec in self:
-            if rec.content_type == 'col_day':
-                total_count = 0
-                rec.total_count = self.env['account.']
+        if self.content_type == 'col_day':
+            total_count = 0
+            invoices_billing = self.env['account.invoice'].sudo().search([('billing_id','!=',False), ('payment_ids.payment_date','=',datetime.datetime.today().date())])
+            for invoice_billing in invoices_billing:
+                total_count = total_count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',datetime.datetime.today().date())]).mapped('amount'))
+            rec.total_count = total_count
+        elif self.content_type == 'col_mo':
+            total_count = 0
+            invoices_billing = self.env['account.invoice'].sudo().search([('billing_id','!=',False), ('payment_ids.payment_date','>=',datetime.datetime.today().date(day=1)), ('payment_ids.payment_date','<',datetime.datetime.today().date(day=1)+relativedelta(month=1))])
+            for invoice_billing in invoices_billing:
+                total_count = total_count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','>=',datetime.datetime.today().date(day=1)), ('payment_date','<',datetime.datetime.today().date(day=1)+relativedelta(month=1))]).mapped('amount'))
+            rec.total_count = total_count
                 
                 
         
@@ -60,43 +78,54 @@ class AccountBillingDashboard(models.Model):
             self.kanban_dashboard_graph = json.dumps(self.get_kanban_dashboard_line_datas()) if self.kanban_dashboard_graph_type == 'line' else json.dumps(self.get_kanban_dashboard_bar_datas())
 
     def _graph_title_and_key(self):
-        return ['', _('Leads')]
+        return ['', _('Collection')]
 
     @api.one
     def get_kanban_dashboard_bar_datas(self):
-        data = []
-        today = datetime.today()
-        date_start = date(month=today.month, year=today.year, day=1)
-        date_end = date(month=today.month+1, year=today.year, day=1)
-        num_days = (date_end - relativedelta(days=1)).day
-        for i in range(1,num_days+1):
-            get_date = date(month=today.month, year=today.year, day=i)
-            get_date_short_name = get_date.strftime("%d")
-            get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
-            get_date_end = datetime(month=today.month, year=today.year, day=i, hour=23, minute=59, second=59)
-            count = len(self.env['crm.lead'].sudo().search([('create_date','>=',get_date_start.strftime("%Y-%m-%d %H:%M:%S")), ('create_date','<=',get_date_end.strftime("%Y-%m-%d %H:%M:%S"))]))
-            data.append({'label':get_date_short_name,'value': count, 'type': 'past' if i<today.day else 'future'})
-        [graph_title, graph_key] = self._graph_title_and_key()
-        return {'values': data, 'title': graph_title, 'key': graph_key}
+        if self.content_type == 'col_mo':
+            data = []
+            today = datetime.today()
+            date_start = date(month=today.month, year=today.year, day=1)
+            date_end = date(month=today.month+1, year=today.year, day=1)
+            num_days = (date_end - relativedelta(days=1)).day
+            for i in range(1,num_days+1):
+                get_date = date(month=today.month, year=today.year, day=i)
+                get_date_short_name = get_date.strftime("%d")
+                get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
+                #get_date_end = datetime(month=today.month, year=today.year, day=i, hour=23, minute=59, second=59)
+                invoices_billing = self.env['account.invoice'].sudo().search([('billing_id','!=',False), ('payment_ids.payment_date','=',get_date_start)])
+                count = 0
+                for invoice_billing in invoices_billing:
+                    count = count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',get_date_start)]).mapped('amount'))
+                data.append({'label':get_date_short_name,'value': count, 'type': 'past' if i<today.day else 'future'})
+            [graph_title, graph_key] = self._graph_title_and_key()
+            return {'values': data, 'title': graph_title, 'key': graph_key}
+        return True
 
     @api.one
     def get_kanban_dashboard_line_datas(self):
-        data = []
-        today = datetime.today()
-        date_start = date(month=today.month, year=today.year, day=1)
-        date_end = date(month=today.month+1, year=today.year, day=1)
-        num_days = (date_end - relativedelta(days=1)).day
-        for i in range(1,num_days+1):
-            get_date = date(month=today.month, year=today.year, day=i)
-            get_date_name = get_date.strftime("%d %B %Y")
-            get_date_short_name = get_date.strftime("%d-%b")
-            get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
-            get_date_end = datetime(month=today.month, year=today.year, day=i, hour=23, minute=59, second=59)
-            count = len(self.env['crm.lead'].sudo().search([('create_date','>=',get_date_start.strftime("%Y-%m-%d %H:%M:%S")), ('create_date','<=',get_date_end.strftime("%Y-%m-%d %H:%M:%S"))]))
-            data.append({'x': get_date_short_name, 'y':count, 'name': get_date_name})
-        [graph_title, graph_key] = self._graph_title_and_key()
-        color = '#875A7B' if '+e' in version else '#7c7bad'
-        return {'values': data, 'title': graph_title, 'key': graph_key, 'area': True, 'color': color}
+        if self.content_type == 'col_mo':
+            data = []
+            today = datetime.today()
+            date_start = date(month=today.month, year=today.year, day=1)
+            date_end = date(month=today.month+1, year=today.year, day=1)
+            num_days = (date_end - relativedelta(days=1)).day
+            for i in range(1,num_days+1):
+                get_date = date(month=today.month, year=today.year, day=i)
+                get_date_name = get_date.strftime("%d %B %Y")
+                get_date_short_name = get_date.strftime("%d-%b")
+                get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
+                #get_date_end = datetime(month=today.month, year=today.year, day=i, hour=23, minute=59, second=59)
+                #count = len(self.env['crm.lead'].sudo().search([('create_date','>=',get_date_start.strftime("%Y-%m-%d %H:%M:%S")), ('create_date','<=',get_date_end.strftime("%Y-%m-%d %H:%M:%S"))]))
+                invoices_billing = self.env['account.invoice'].sudo().search([('billing_id','!=',False), ('payment_ids.payment_date','=',get_date_start)])
+                count = 0
+                for invoice_billing in invoices_billing:
+                    count = count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',get_date_start)]).mapped('amount'))
+                data.append({'x': get_date_short_name, 'y':count, 'name': get_date_name})
+            [graph_title, graph_key] = self._graph_title_and_key()
+            color = '#875A7B' if '+e' in version else '#7c7bad'
+            return {'values': data, 'title': graph_title, 'key': graph_key, 'area': True, 'color': color}
+        return True
 
     @api.one
     def _get_name(self):
@@ -110,4 +139,4 @@ class AccountBillingDashboard(models.Model):
     type = fields.Selection([('combined','Combined'), ('text','Text'), ('graph','Graph')], string="Dashboard Content Type", default="combined")
     kanban_dashboard_graph = fields.Text(compute="_kanban_dashboard_graph")
     kanban_dashboard_graph_type = fields.Selection([('line','Line'),('bar','Bar')], string="Graph Type", default='line')    
-    content_type = fields.Selection([('lead_mo','Monthly Leads'), ('lead_all','Total Leads')], string="Content Type")
+    content_type = fields.Selection([('col_day','Daily Collection'), ('col_mo','Daily Collection')], string="Content Type")
