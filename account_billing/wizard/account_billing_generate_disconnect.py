59cb45d79f136adfb349af6a284762ea722eddb8
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-17 10:32:33 +0800

    Update account_billing_generate_disconnect.py

--- account_billing/wizard/account_billing_generate_disconnect.py
+++ account_billing/wizard/account_billing_generate_disconnect.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+import datetime
 
 class AccountBillingGenerateDisconnect(models.TransientModel):
     _name = 'account.billing.generate.disconnect'
@@ -6,8 +7,8 @@ class AccountBillingGenerateDisconnect(models.TransientModel):
     @api.multi
     def generate(self):
         self.ensure_one()
-        invoices = self.env['account.billing']._recurring_create_invoice()
-        ids = []
-        for invoice in invoices:
-            ids.append(invoice.id)
-        return self.env.ref('account_billing.report_account_invoice_bill_report').report_action(ids)
+        invoice_ids = self.env['account.invoice'].sudo().search([('state','=','open'), ('billing_id','!=',False), ('billing_id.state', '=', 'open'), ('date_due','<',datetime.datetime.today().date())])
+        if invoice_ids and len(invoice_ids)>0:
+            return self.env.ref('account_billing.report_account_invoice_disconnect_report').report_action(ids)
+        else
+            raise UserError("There are no overdue invoices!")
