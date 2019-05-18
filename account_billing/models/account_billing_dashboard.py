de602d2b7bcada74c10ee9e8145c1c8062fa0ca5
ithinksols <50433071+ithinksols@users.noreply.github.com>
2019-05-18 17:02:12 +0800

    Update account_billing_dashboard.py

--- account_billing/models/account_billing_dashboard.py
+++ account_billing/models/account_billing_dashboard.py
@@ -56,6 +56,46 @@ class AccountBillingDashboard(models.Model):
                         rec.sudo().unlink()
         else:
             self.env['account.billing.dashboard'].sudo().create({'name': "This Month's Collection", 'type': 'combined', 'content_type': 'col_mo'})
+        if records:
+            if len(records) == 0:
+                self.env['account.billing.dashboard'].sudo().create({'name': "Today's Dues Collection (Water and Association Dues)", 'type': 'text', 'content_type': 'dues_col_day'})
+            elif len(records) > 1:
+                count = 0
+                for rec in records:
+                    if count > 0:
+                        rec.sudo().unlink()
+        else:
+            self.env['account.billing.dashboard'].sudo().create({'name': "Today's Dues Collection (Water and Association Dues)", 'type': 'text', 'content_type': 'dues_col_day'})
+        if records:
+            if len(records) == 0:
+                self.env['account.billing.dashboard'].sudo().create({'name': "This Month's Dues Collection (Water and Association Dues)", 'type': 'combined', 'content_type': 'dues_col_mo'})
+            elif len(records) > 1:
+                count = 0
+                for rec in records:
+                    if count > 0:
+                        rec.sudo().unlink()
+        else:
+            self.env['account.billing.dashboard'].sudo().create({'name': "This Month's Dues Collection (Water and Association Dues)", 'type': 'combined', 'content_type': 'dues_col_mo'})
+        if records:
+            if len(records) == 0:
+                self.env['account.billing.dashboard'].sudo().create({'name': "Today's Other Collection (Rentals, Water meter charges, etc.)", 'type': 'text', 'content_type': 'other_col_day'})
+            elif len(records) > 1:
+                count = 0
+                for rec in records:
+                    if count > 0:
+                        rec.sudo().unlink()
+        else:
+            self.env['account.billing.dashboard'].sudo().create({'name': "Today's Other Collection (Rentals, Water meter charges, etc.)", 'type': 'text', 'content_type': 'other_col_day'})
+        if records:
+            if len(records) == 0:
+                self.env['account.billing.dashboard'].sudo().create({'name': "This Month's Other Collection (Rentals, Water meter charges, etc.)", 'type': 'combined', 'content_type': 'other_col_mo'})
+            elif len(records) > 1:
+                count = 0
+                for rec in records:
+                    if count > 0:
+                        rec.sudo().unlink()
+        else:
+            self.env['account.billing.dashboard'].sudo().create({'name': "This Month's Other Collection (Rentals, Water meter charges, etc.)", 'type': 'combined', 'content_type': 'other_col_mo'})
         self.env['account.billing.dashboard'].sudo().search([('content_type','=',False)]).unlink()
                     
                     
@@ -63,14 +103,42 @@ class AccountBillingDashboard(models.Model):
     def _count(self):
         if self.content_type == 'col_day':
             total_count = 0
-            invoices_billing = self.env['account.invoice'].sudo().search([('billing_id','!=',False), ('payment_ids.payment_date','=',datetime.today().date())])
+            invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('payment_ids.payment_date','=',datetime.today().date())])
             for invoice_billing in invoices_billing:
                 total_count = total_count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',datetime.today().date())]).mapped('amount'))
             self.total_count = total_count
             self.short_count = self.human_format(total_count)
         elif self.content_type == 'col_mo':
             total_count = 0
-            invoices_billing = self.env['account.invoice'].sudo().search([('billing_id','!=',False), ('payment_ids.payment_date','>=',datetime.today().date().replace(day=1)), ('payment_ids.payment_date','<',datetime.today().date().replace(day=1)+relativedelta(months=1))])
+            invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('payment_ids.payment_date','>=',datetime.today().date().replace(day=1)), ('payment_ids.payment_date','<',datetime.today().date().replace(day=1)+relativedelta(months=1))])
+            for invoice_billing in invoices_billing:
+                total_count = total_count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','>=',datetime.today().date().replace(day=1)), ('payment_date','<',datetime.today().date().replace(day=1)+relativedelta(months=1))]).mapped('amount'))
+            self.total_count = total_count
+            self.short_count = self.human_format(total_count)
+        elif self.content_type == 'dues_col_day':
+            total_count = 0
+            invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('billing_id','!=',False), ('payment_ids.payment_date','=',datetime.today().date())])
+            for invoice_billing in invoices_billing:
+                total_count = total_count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',datetime.today().date())]).mapped('amount'))
+            self.total_count = total_count
+            self.short_count = self.human_format(total_count)
+        elif self.content_type == 'dues_col_mo':
+            total_count = 0
+            invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('billing_id','!=',False), ('payment_ids.payment_date','>=',datetime.today().date().replace(day=1)), ('payment_ids.payment_date','<',datetime.today().date().replace(day=1)+relativedelta(months=1))])
+            for invoice_billing in invoices_billing:
+                total_count = total_count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','>=',datetime.today().date().replace(day=1)), ('payment_date','<',datetime.today().date().replace(day=1)+relativedelta(months=1))]).mapped('amount'))
+            self.total_count = total_count
+            self.short_count = self.human_format(total_count)
+        elif self.content_type == 'other_col_day':
+            total_count = 0
+            invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('billing_id','==',False), ('payment_ids.payment_date','=',datetime.today().date())])
+            for invoice_billing in invoices_billing:
+                total_count = total_count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',datetime.today().date())]).mapped('amount'))
+            self.total_count = total_count
+            self.short_count = self.human_format(total_count)
+        elif self.content_type == 'other_col_mo':
+            total_count = 0
+            invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('billing_id','==',False), ('payment_ids.payment_date','>=',datetime.today().date().replace(day=1)), ('payment_ids.payment_date','<',datetime.today().date().replace(day=1)+relativedelta(months=1))])
             for invoice_billing in invoices_billing:
                 total_count = total_count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','>=',datetime.today().date().replace(day=1)), ('payment_date','<',datetime.today().date().replace(day=1)+relativedelta(months=1))]).mapped('amount'))
             self.total_count = total_count
@@ -104,8 +172,41 @@ class AccountBillingDashboard(models.Model):
                 get_date = date(month=today.month, year=today.year, day=i)
                 get_date_short_name = get_date.strftime("%d")
                 get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
-                #get_date_end = datetime(month=today.month, year=today.year, day=i, hour=23, minute=59, second=59)
-                invoices_billing = self.env['account.invoice'].sudo().search([('billing_id','!=',False), ('payment_ids.payment_date','=',get_date_start)])
+                invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('payment_ids.payment_date','=',get_date_start)])
+                count = 0
+                for invoice_billing in invoices_billing:
+                    count = count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',get_date_start)]).mapped('amount'))
+                data.append({'label':get_date_short_name,'value': count, 'type': 'past' if i<today.day else 'future'})
+            [graph_title, graph_key] = self._graph_title_and_key()
+            return {'values': data, 'title': graph_title, 'key': graph_key}
+        elif self.content_type == 'dues_col_mo':
+            data = []
+            today = datetime.today()
+            date_start = date(month=today.month, year=today.year, day=1)
+            date_end = date(month=today.month+1, year=today.year, day=1)
+            num_days = (date_end - relativedelta(days=1)).day
+            for i in range(1,num_days+1):
+                get_date = date(month=today.month, year=today.year, day=i)
+                get_date_short_name = get_date.strftime("%d")
+                get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
+                invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('billing_id','!=',False),('payment_ids.payment_date','=',get_date_start)])
+                count = 0
+                for invoice_billing in invoices_billing:
+                    count = count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',get_date_start)]).mapped('amount'))
+                data.append({'label':get_date_short_name,'value': count, 'type': 'past' if i<today.day else 'future'})
+            [graph_title, graph_key] = self._graph_title_and_key()
+            return {'values': data, 'title': graph_title, 'key': graph_key}
+        elif self.content_type == 'other_col_mo':
+            data = []
+            today = datetime.today()
+            date_start = date(month=today.month, year=today.year, day=1)
+            date_end = date(month=today.month+1, year=today.year, day=1)
+            num_days = (date_end - relativedelta(days=1)).day
+            for i in range(1,num_days+1):
+                get_date = date(month=today.month, year=today.year, day=i)
+                get_date_short_name = get_date.strftime("%d")
+                get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
+                invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('billing_id','==',False),('payment_ids.payment_date','=',get_date_start)])
                 count = 0
                 for invoice_billing in invoices_billing:
                     count = count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',get_date_start)]).mapped('amount'))
@@ -127,9 +228,45 @@ class AccountBillingDashboard(models.Model):
                 get_date_name = get_date.strftime("%d %B %Y")
                 get_date_short_name = get_date.strftime("%d-%b")
                 get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
-                #get_date_end = datetime(month=today.month, year=today.year, day=i, hour=23, minute=59, second=59)
-                #count = len(self.env['crm.lead'].sudo().search([('create_date','>=',get_date_start.strftime("%Y-%m-%d %H:%M:%S")), ('create_date','<=',get_date_end.strftime("%Y-%m-%d %H:%M:%S"))]))
-                invoices_billing = self.env['account.invoice'].sudo().search([('billing_id','!=',False), ('payment_ids.payment_date','=',get_date_start)])
+                invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('payment_ids.payment_date','=',get_date_start)])
+                count = 0
+                for invoice_billing in invoices_billing:
+                    count = count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',get_date_start)]).mapped('amount'))
+                data.append({'x': get_date_short_name, 'y':count, 'name': get_date_name})
+            [graph_title, graph_key] = self._graph_title_and_key()
+            color = '#875A7B' if '+e' in version else '#7c7bad'
+            return {'values': data, 'title': graph_title, 'key': graph_key, 'area': True, 'color': color}
+        elif self.content_type == 'dues_col_mo':
+            data = []
+            today = datetime.today()
+            date_start = date(month=today.month, year=today.year, day=1)
+            date_end = date(month=today.month+1, year=today.year, day=1)
+            num_days = (date_end - relativedelta(days=1)).day
+            for i in range(1,num_days+1):
+                get_date = date(month=today.month, year=today.year, day=i)
+                get_date_name = get_date.strftime("%d %B %Y")
+                get_date_short_name = get_date.strftime("%d-%b")
+                get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
+                invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('billing_id','!=',False),('payment_ids.payment_date','=',get_date_start)])
+                count = 0
+                for invoice_billing in invoices_billing:
+                    count = count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',get_date_start)]).mapped('amount'))
+                data.append({'x': get_date_short_name, 'y':count, 'name': get_date_name})
+            [graph_title, graph_key] = self._graph_title_and_key()
+            color = '#875A7B' if '+e' in version else '#7c7bad'
+            return {'values': data, 'title': graph_title, 'key': graph_key, 'area': True, 'color': color}
+        elif self.content_type == 'other_col_mo':
+            data = []
+            today = datetime.today()
+            date_start = date(month=today.month, year=today.year, day=1)
+            date_end = date(month=today.month+1, year=today.year, day=1)
+            num_days = (date_end - relativedelta(days=1)).day
+            for i in range(1,num_days+1):
+                get_date = date(month=today.month, year=today.year, day=i)
+                get_date_name = get_date.strftime("%d %B %Y")
+                get_date_short_name = get_date.strftime("%d-%b")
+                get_date_start = datetime(month=today.month, year=today.year, day=i, hour=0, minute=0, second=0)
+                invoices_billing = self.env['account.invoice'].sudo().search([('state','in',['open','in_payment','paid']),('billing_id','==',False),('payment_ids.payment_date','=',get_date_start)])
                 count = 0
                 for invoice_billing in invoices_billing:
                     count = count + sum(self.env['account.payment'].sudo().search([('id','in',invoice_billing.payment_ids.ids), ('payment_date','=',get_date_start)]).mapped('amount'))
@@ -147,4 +284,7 @@ class AccountBillingDashboard(models.Model):
     type = fields.Selection([('combined','Combined'), ('text','Text'), ('graph','Graph')], string="Dashboard Content Type", default="combined")
     kanban_dashboard_graph = fields.Text(compute="_kanban_dashboard_graph")
     kanban_dashboard_graph_type = fields.Selection([('line','Line'),('bar','Bar')], string="Graph Type", default='line')    
-    content_type = fields.Selection([('col_day','Daily Collection'), ('col_mo','Daily Collection')], string="Content Type")
+    content_type = fields.Selection([('col_day','Daily Collection'), ('col_mo','Monthly Collection'),
+                                     ('dues_col_day','Daily Dues Collection'), ('dues_col_mo','Monthly Dues Collection'),
+                                     ('other_col_day','Daily Other Collection'), ('other_col_mo','Monthly Other Collection
+                                    ], string="Content Type")
